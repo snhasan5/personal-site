29f878317556b52927213e5c32bbcf2eef1abcc9
snhasan5 <snhasan2005>
2024-08-05 14:06:35 +0530

    deploy

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        argt: ['Playwrite AR']
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
